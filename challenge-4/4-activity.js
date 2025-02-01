7f9bf8d149ca093ae64863ffb06e149c4a64cea1
Satvik <sharmasatvik43@gmail.com>
2025-02-01 22:31:55 +0530

    Challenge 4 is done

---
+++ challenge-4/4-activity.js
@@ -0,0 +1,17 @@
+// Task 7
+// Write a program to print a pattern using nested for loops:
+//     *
+//     *  *
+//     *  *  *
+//     *  *  *  *
+//     *  *  *  *  *
+
+let rows = 5;
+
+for (let i = 1; i <= rows; i++) {
+  let pattern = "";
+  for (let j = 1; j <= i; j++) {
+    pattern += "* ";
+  }
+  console.log(pattern);
+}
