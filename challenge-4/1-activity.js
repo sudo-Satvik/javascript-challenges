54cc787a999586bd33563e5651c4d2696dc151c0
Satvik <sharmasatvik43@gmail.com>
2025-02-01 20:32:03 +0530

    Add Challenge 4 activities and solutions for loops in JavaScript

---
+++ challenge-4/1-activity.js
@@ -0,0 +1,12 @@
+// Task 1
+// Write a program to print numbers from 1 to 10 using a for loop.
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1);
+}
+
+// Task 2
+// Write a program to print the multiplication table of 5 using a for loop.
+let table = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${table} x ${i} = ${table * i}`);
+}
