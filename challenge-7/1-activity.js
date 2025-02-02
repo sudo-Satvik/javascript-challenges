247c5ae5df7405c286ae5b058f26fceb0c4b531f
Satvik <sharmasatvik43@gmail.com>
2025-02-02 23:28:41 +0530

    Add Challenge 7 activities and solutions for object creation and property access in JavaScript

---
+++ challenge-7/1-activity.js
@@ -0,0 +1,14 @@
+// Task 1
+// Create an object representing a book with properties like title, author, and year, and log the object to the console.
+const book = {
+  title: "Diary of Anne Frank",
+  author: "Anne Frank",
+  year: 1947,
+};
+console.log(book);
+
+// Task 2
+// Access and log the title and author properties of the book object.
+console.log(
+  `The title of the book is ${book.title} which is written by ${book.author}`
+);
